4640bbb85f34ecd35a24c19b098d65f7129801d5
mukul <mukul.atreya@gmail.com>
2022-02-19 15:58:03 +0545

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import helmet from 'helmet';
-import * as csurf from 'csurf';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
